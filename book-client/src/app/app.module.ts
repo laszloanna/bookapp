0fb5af4ba89aefd28bd271e91937f88293044478
László Anna <me8ppk@inf.elte.hu>
2021-09-24 22:09:32 +0200

    develop book-list component

--- book-client/src/app/app.module.ts
+++ book-client/src/app/app.module.ts
@@ -21,6 +21,7 @@ import {MatDialogModule} from '@angular/material/dialog';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatSelectModule} from '@angular/material/select';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -64,7 +65,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    MatSelectModule
+    MatSelectModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- book-client/src/app/book-list/book-list.component.ts
+++ book-client/src/app/book-list/book-list.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookListComponent implements OnInit {
 
+  books = [
+    {_id: 1, title: "First book", author: "asd1", summary: "Brief summary of the first book", genre: "fantasy"},
+    {_id: 2, title: "Second book", author: "asd2", summary: "Brief summary of the second book", genre: "romance"},
+    {_id: 3, title: "Third book", author: "asd3", summary: "Brief summary of the third book", genre: "horror"},
+    {_id: 4, title: "Fourth book", author: "asd4", summary: "Brief summary of the fourth book", genre: "thriller"}
+  ];
+
+
+
   constructor() { }
 
   ngOnInit(): void {
